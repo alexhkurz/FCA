8740f6f5e2192983dc19d164b34e95f111003b13
Alexander Kurz <alexhkurz@gmail.com>
2024-06-02 14:37:46 -0700

    Moved the 'down' method and added the 'up' method in the Relation class.

--- main.js
+++ main.js
@@ -5,25 +5,6 @@ class Relation {
         this.triples = triples;
     }
 
-    down(weightedUpset) {
-        let downUp = [];
-        this.getObjects().forEach(object => {
-            let minVal = Infinity;
-            this.getAttributes().forEach(attribute => {
-                let incidenceRelTriple = this.triples.find(triple => this.getObject(triple) === object && this.getAttribute(triple) === attribute);
-                let pair = weightedUpset.find(wu => wu[0] === attribute);
-                let weight = pair ? pair[1] : 0; // if pair is undefined, set weight to 0
-                let relationVal = incidenceRelTriple ? this.getValue(incidenceRelTriple) : 0; // if relation undefined, set value to 0
-                let val = truncatedDiv(relationVal, weight); 
-                if (val < minVal) {
-                    minVal = val;
-                }
-            });
-            downUp.push([object, minVal]);
-        });
-        return downUp;
-    }
-
     getObjects() {
         return [...new Set(this.triples.map(triple => triple[0]))];
     }
@@ -46,6 +27,44 @@ class Relation {
 
     getValue(triple) {
         return triple[2];
+    }
+
+    down(weightedUpset) {
+        let downUp = [];
+        this.getObjects().forEach(object => {
+            let minVal = Infinity;
+            this.getAttributes().forEach(attribute => {
+                let incidenceRelTriple = this.triples.find(triple => this.getObject(triple) === object && this.getAttribute(triple) === attribute);
+                let pair = weightedUpset.find(wu => wu[0] === attribute);
+                let weight = pair ? pair[1] : 0; // if pair is undefined, set weight to 0
+                let relationVal = incidenceRelTriple ? this.getValue(incidenceRelTriple) : 0; // if relation undefined, set value to 0
+                let val = truncatedDiv(relationVal, weight); 
+                if (val < minVal) {
+                    minVal = val;
+                }
+            });
+            downUp.push([object, minVal]);
+        });
+        return downUp;
+    }
+
+    up(weightedDownset) {
+        let upOfDown = [];
+        this.getAttributes().forEach(attribute => {
+            let minVal = Infinity;
+            this.getObjects().forEach(object => {
+                let incidenceRelTriple = this.triples.find(triple => this.getAttribute(triple) === attribute && this.getObject(triple) === object);
+                let pair = weightedDownset.find(wu => wu[0] === object);
+                let weight = pair ? pair[1] : 0; // if pair is undefined, set weight to 0
+                let relationVal = incidenceRelTriple ? this.getValue(incidenceRelTriple) : 0; // if relation undefined, set value to 0
+                let val = truncatedDiv(relationVal, weight); 
+                if (val < minVal) {
+                    minVal = val;
+                }
+            });
+            upOfDown.push([attribute, minVal]);
+        });
+        return downUp;
     }
 }
 
