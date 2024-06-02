002cf1736308d87c8e43ef97820dfafbc9c770c1
Alexander Kurz <alexhkurz@gmail.com>
2024-06-01 19:33:57 -0700

    Removed 'x4' from objects array and updated relation array, added weightedUpset array.

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 let attributes = ['a1', 'a2', 'a3'];
 
-let objects = ['x1', 'x2', 'x3', 'x4'];
+let objects = ['x1', 'x2', 'x3'];
 
 let relation = [
     ['x1', 'a1'],
     ['x1', 'a2'],
-    ['x3', 'a3'],
-    ['x4', 'a1'],
-    ['x4', 'a2']
-];
+    ['x2', 'a2'],
+    ['x3', 'a3']];
+
+let weightedUpset = [
+    ['a1', 0.5],
+    ['a2', 0.5],
+    ['a3', 0.5],
+]
 
 
 
